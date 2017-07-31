c44a51b80eb9745bd2160a84a468f0214ef3d7b2
hallover <holiverbyu@gmail.com>
2017-07-31 14:35:21 -0600

    Removed deprecated file

--- code/pulldata.sh
+++ code/pulldata.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-awk '/free energy/ {e = $5}; END {print e}' OUTCAR >> temp.txt
+#awk '/free energy/ {e = $5}; END {print e}' OUTCAR >> temp.txt
 
-for directory in /fslhome/holiver2/work/vasp/alloydatabase/finished/0-CdCoN/10frzkpts/*/OUTCAR; do
-    awk '/free energy/ {e = $5}; END {print e}' OUTCAR >> /fslhome/holiver2/work/vasp/alloydatabase/finished/0-CdCoN/10frzkpts/temp.txt
-done
+#for directory in /fslhome/holiver2/work/vasp/alloydatabase/finished/0-CdCoN/10frzkpts/*/OUTCAR; do
+ #   awk '/free energy/ {e = $5}; END {print e}' OUTCAR >> /fslhome/holiver2/work/vasp/alloydatabase/finished/0-CdCoN/10frzkpts/temp.txt
+#done
+
+cd /fslhome/holiver2/work/vasp/alloydatabase/finished/0-CdCoN/
+find . -name OUTCAR -exec awk '/free energy/ {energy = $5}; END {print FILENAME " " energy}'{}\;
+
+#for directory in ls -d /fslhome/holiver2/work/vasp/alloydatabase/finished/0-CdCoN/
+#do
